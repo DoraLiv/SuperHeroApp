1aa81fe75345a7944485e705cf46d8fc15177062
DoraLiv <dora.livkisa@gmail.com>
2022-02-24 20:52:33 +0200

    Added Hero data type

--- SuperHeroApp/Program.cs
+++ SuperHeroApp/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace SuperHeroApp
+{ internal class Program
+    { static void Main(string[] args)
+        { string name = "Anti";
+            int age = 428;
+            string heroPower1, heroPower2, heroPower3;
+            heroPower1 = "Telepathically breaking and blowing up things";
+            heroPower2 = "X-ray vision";
+            heroPower3 = "Eating unlimited amounts of chocolate";
+            Console.WriteLine("~~~~~~~~*****~~~~~~~*****~~~~~~~*****~~~~~~~");
+            Console.WriteLine("\n\n        THE LEGEND OF THE ANTI HERO");
+            Console.WriteLine("\n\n~~~~~~~~*****~~~~~~~*****~~~~~~~*****~~~~~~~");
+            Console.WriteLine($"\n\nName : {name}");
+            Console.WriteLine("\nAge : " + age);
+            Console.WriteLine($"\nHero Powers :\n  *{heroPower1} \n  *{heroPower2} \n  *{heroPower3}\n\n\n\n\n");                       }
+    }
+}
