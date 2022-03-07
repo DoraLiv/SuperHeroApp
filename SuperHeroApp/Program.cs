f1a61dfe7ef5cce97b73072cc499d65b984cb82f
DoraLiv <dora.livkisa@gmail.com>
2022-03-07 21:17:58 +0200

    Work and cookies

--- SuperHeroApp/Program.cs
+++ SuperHeroApp/Program.cs
@@ -7,12 +7,44 @@ namespace SuperHeroApp
             string heroPower1, heroPower2, heroPower3;
             heroPower1 = "Telepathically breaking and blowing up things";
             heroPower2 = "X-ray vision";
-            heroPower3 = "Eating unlimited amounts of chocolate";
+            heroPower3 = "Eating unlimited amounts of cookies";
+            double salary = 1452;
+            bool isevil = false;
+            int deedTimeInHours1 = 3;
+            int deedTimeInHours2 = 4;
+            int deedTimeInHours3 = 1;
+            int hourlycookies = 5;
+           
             Console.WriteLine("~~~~~~~~*****~~~~~~~*****~~~~~~~*****~~~~~~~");
             Console.WriteLine("\n\n        THE LEGEND OF THE ANTI HERO");
             Console.WriteLine("\n\n~~~~~~~~*****~~~~~~~*****~~~~~~~*****~~~~~~~");
             Console.WriteLine($"\n\nName : {name}");
             Console.WriteLine("\nAge : " + age);
-            Console.WriteLine($"\nHero Powers :\n  *{heroPower1} \n  *{heroPower2} \n  *{heroPower3}\n\n\n\n\n");                       }
+            Console.WriteLine($"\nHero Powers :\n  *{heroPower1} \n  *{heroPower2} \n  *{heroPower3}\n\n");
+
+            //work and cookies
+
+            double cookieprice = 1.35;
+            double numofcookies = Math.Floor(salary / cookieprice);
+            double dailysalary = Math.Round(salary / 30, 2);
+            double workintotal = deedTimeInHours1 + deedTimeInHours2 +deedTimeInHours3;
+            double workaverage = Math.Round(workintotal / 3,2);
+            double cookiesearned = workintotal * hourlycookies;
+            double cookiesearnedperdeed = Math.Round(workaverage * hourlycookies,2);
+            Console.WriteLine("~~~~~~~~*****~~~~~~~*****~~~~~~~*****~~~~~~~\n");
+            Console.WriteLine($"Anti can buy {numofcookies} cookies");
+            Console.WriteLine($"Anti's daily salary is {dailysalary} monies. ");
+            if (isevil == true)
+            {
+                Console.WriteLine("Anti will destroy the Cookie city!");
+            }
+            else
+            {
+                Console.WriteLine("Anti is not evil, he would not intentionally hurt the Cookie city.");
+            }
+            Console.WriteLine($"Anti works {workintotal} hours a day.\nIn average it takes him {workaverage} hours to do one deed.");
+            Console.WriteLine($"Anti earns {cookiesearned} cookies for his work, averaging {cookiesearnedperdeed} cookies per deed.");
+
+            }
     }
 }
